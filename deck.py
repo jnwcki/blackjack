700f743f52e469ddff350ca5f19fe37b68f07134
jnwcki <jude.nowicki@gmail.com>
2016-02-11 13:46:12 -0500

    Rewrote deck. Now uses list comprehension, only needs to return one card at a time

--- deck.py
+++ deck.py
@@ -4,30 +4,9 @@ from card import Card
 
 class Deck:
     def __init__(self):
+        self.deck = [Card(rank, suit) for rank in range(1, 14) for suit in range(0, 3)]
+        random.shuffle(self.deck)
 
-        self.deck = Card()
-        self.working_deck = self.new_deck()
-
-    def new_deck(self):
-        # self.new_deck = [[suit, rank for suit in Card.suit] for rank in Card.rank])
-        new_deck = []
-        for s in self.deck.suit:
-            for r, v in self.deck.rank.items():
-                new_deck.append([v, r + " of " + s])
-        random.shuffle(new_deck)
-        return new_deck
-
-
-
-
-    def hit_one(self):
-        card = self.working_deck.pop()
+    def hit(self):
+        card = self.deck.pop()
         return card
-
-    def hit_two(self):
-        hit_two = []
-        for _ in range(2):
-            self.working_deck.pop()
-
-            hit_two.append(self.working_deck.pop())
-        return hit_two
